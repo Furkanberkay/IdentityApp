f9fa7b6b2ba2787c54133ac50d4c8f22309c873d
Furkanberkay <furkanberkayozcan@hotmail.com>
2025-08-28 11:03:37 +0300

    Update assembly informational version and cache; add AppUser model class

--- IdentityApp/obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
+++ IdentityApp/obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d39617efb98e2dec957f098638540153e777764")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf8cfa6c7eb0b2ffc582d53322ae2d1da6d78f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
