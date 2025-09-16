61528f8ba3efe11e8325f0635afa8a6354d57e62
FurkanBerkay <furkanberkayozcan@hotmail.com>
2025-09-16 23:44:19 +0300

    Implement role editing functionality; add Edit view and RoleUsersTagHelper for user-role association

--- IdentityApp/Controllers/RolesController.cs
+++ IdentityApp/Controllers/RolesController.cs
@@ -7,9 +7,12 @@ namespace IdentityApp.Controllers
     public class RolesController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-        public RolesController(RoleManager<AppRole> roleManager)
+        private readonly UserManager<AppUser> _userManager;
+
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -45,6 +48,68 @@ namespace IdentityApp.Controllers
             }
             return View(model);
         }
-        
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (role.Name == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+            return View(role);
+
+        }
+
+        [HttpPost]
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppRole model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                role.Name = model.Name;
+
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+
+            if (role.Name == null)
+            {
+                return View(role);
+            }
+
+            // role null olmadığı garanti → güvenli
+            ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+            return View(role);
+        }
     }
 }
